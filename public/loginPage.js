480701e6b2c7c0f98f78cdb2041382c20fef26ba
BlueberryDari <avlertaver@gmail.com>
2026-07-04 18:29:39 +0300

    beautified

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,31 +1,34 @@
 "use strict";
 //формa регистрации
 const userForm = new UserForm();
-
-    //попытка авторизации
-userForm.loginFormCallback = function({login, password}) {
-ApiConnector.login({login, password}, function (response) {
-    console.log(response);
-    if (response.success) {
-        location.reload();
-    } else {
-        userForm.setLoginErrorMessage(response.error);
-    }
-});
+//попытка авторизации
+userForm.loginFormCallback = function({
+    login,
+    password
+}) {
+    ApiConnector.login({
+        login,
+        password
+    }, function(response) {
+        console.log(response);
+        if (response.success) {
+            location.reload();
+        } else {
+            userForm.setLoginErrorMessage(response.error);
+        }
+    });
 };
 /*попытка регистрации, та же логика, запись стрелочной ф-ии, 
 вместо расписанного {login, password} - data.*/
 userForm.registerFormCallback = (data) => {
-ApiConnector.register(data, response => {
-if (response.success) {
-    location.reload();
-} else {
-    userForm.setRegisterErrorMessage(response.error);
+    ApiConnector.register(data, response => {
+        if (response.success) {
+            location.reload();
+        } else {
+            userForm.setRegisterErrorMessage(response.error);
+        }
+    });
 }
-});
-}
-
-
 //работа с сервером
 /*class ApiConnector {
 login({login, password}, callback) {
